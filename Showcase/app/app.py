fb8ce9172448ffe6760bdd8187061ef4722df54a
Anass <anassairad@hotmail.com>
2026-04-03 11:06:25 +0200

    commit

--- Showcase/app/app.py
+++ Showcase/app/app.py
@@ -6,17 +6,17 @@ import numpy as np
 
 
 def user_profile_def(user_id, view_history, programs, programs_tfidf, genres):
-    view_history = pd.read_csv("../data/view_history.csv")
+    view_history = pd.read_csv("data/view_history.csv")
     view_history_user = view_history[view_history["user_id"] == user_id]
 
-    programs = pd.read_csv("../data/programs.csv")
-    view_history_user = pd.merge(view_history_user, pd.read_csv("../data/programs.csv")[["program_id", "category"]], on="program_id", how="left")
+    programs = pd.read_csv("data/programs.csv")
+    view_history_user = pd.merge(view_history_user, pd.read_csv("data/programs.csv")[["program_id", "category"]], on="program_id", how="left")
     if len(view_history_user[view_history_user['category'].isin(genres)]) == 0:
         genres = view_history_user["category"].dropna().unique().tolist()
     view_history_user = view_history_user.loc[view_history_user['category'].isin(genres)]
     view_history_user = view_history_user.drop('category', axis=1)
 
-    programs_tfidf = pd.read_csv("../data/programs_tfidf.csv")
+    programs_tfidf = pd.read_csv("data/programs_tfidf.csv")
 
     user_profile = pd.merge(view_history_user, programs_tfidf, on="program_id", how="left")
     user_profile = user_profile.dropna(axis=0)
@@ -26,6 +26,7 @@ def user_profile_def(user_id, view_history, programs, programs_tfidf, genres):
     user_profile[tfidf_cols] = user_profile[tfidf_cols].multiply(user_profile['listen_ratio'], axis=0)
 
     user_profile = user_profile.groupby('user_id')[tfidf_cols].mean().reset_index()
+    user_profile.to_csv("prototype/data/user_profiles.csv")
     return user_profile
 
 def cosine_similarity_def(user_profile, programs_tfidf):
@@ -47,15 +48,15 @@ def cosine_similarity_def(user_profile, programs_tfidf):
 def top_50_recommendations(user_id, similarity_df, view_history, genres, threshold_amount=50):
     # Adjust this threshold to whatever we need for the exposure fairness and diversity/mmr
     similarity_df = similarity_df.transpose().sort_values(by=user_id, ascending=False)
-    similarity_df = pd.merge(similarity_df, pd.read_csv("../data/programs.csv"), on="program_id", how="left").dropna()
+    similarity_df = pd.merge(similarity_df, pd.read_csv("data/programs.csv"), on="program_id", how="left").dropna()
     similarity_df = similarity_df[~similarity_df['program_id'].isin(view_history[view_history["user_id"] == user_id]['program_id'])]
     similarity_df = similarity_df.loc[similarity_df['category'].isin(genres)]
 
-    return similarity_df.iloc[:threshold_amount]
+    return similarity_df[similarity_df[user_id]>= 0.6]
 
 def exposure_fairness(top_50_recommendations):
-    return top_50_recommendations.sort_values(by="inclusion_score", ascending=False).reset_index(drop=True)
-
+    top_50_recommendations = top_50_recommendations.sort_values(by="inclusion_score", ascending=False).reset_index(drop=True)
+    return top_50_recommendations[:50]
 
 def content_diversity(fairness_df, programs_tfidf, top_n=15, lambda_param=0.5):
     candidate_ids = fairness_df['program_id'].tolist()
@@ -113,9 +114,9 @@ def content_diversity(fairness_df, programs_tfidf, top_n=15, lambda_param=0.5):
     return final_recs
 
 
-def recommendation_content(user_id, view_history, programs, programs_tfidf, genres):
+def recommendation_content(user_id, view_history, programs, programs_tfidf, genres, lambda_param):
     if len(genres) == 0:
-        genres = pd.merge(pd.read_csv("../data/view_history.csv"), pd.read_csv("../data/programs.csv"), on="program_id", how="left")["category"].unique().tolist()
+        genres = pd.merge(pd.read_csv("data/view_history.csv"), pd.read_csv("data/programs.csv"), on="program_id", how="left")["category"].unique().tolist()
 
     user_profile = user_profile_def(user_id, view_history, programs, programs_tfidf, genres)
     
@@ -131,4 +132,4 @@ def recommendation_content(user_id, view_history, programs, programs_tfidf, genr
 
 os.chdir("/home/anass/university/msc_applied_data_science/INFOMPPM/INFOMPPM") # Comment it out when you run it
 
-recommendation_content("U99", pd.read_csv("data/view_history.csv"), pd.read_csv("data/programs.csv"), pd.read_csv("data/programs_tfidf.csv"), ["entertainment"])
+recommendation_content("U99", pd.read_csv("data/view_history.csv"), pd.read_csv("data/programs.csv"), pd.read_csv("data/programs_tfidf.csv"), [], 0.5)
